76ef2914fcf5d94cbd0cdc470f358ebc571243e6
LMartin04 <lindsay.martin2001@gmail.com>
2024-11-23 22:53:19 -0700

    Update ShannonIndex.R

--- ShannonIndex.R
+++ ShannonIndex.R
@@ -1,10 +1,14 @@
-#install and load the required packages 
-install.packages("rio")
-library(rio)
+#install and load the required package
 library(vegan)
 
 #calculate shannon diversity index using vegan package
+
+#insert list of species counts
 species_counts <- c()
+
+#calculate shannon biodiveristy index using the vegan package
 shannon_diversity_vegan <- diversity(species_counts, index="shannon")
+
+#shows the calculated value
 print(shannon_diversity_vegan)
 
